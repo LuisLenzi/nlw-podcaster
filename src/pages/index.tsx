7d75581eb51d9a84fdb711844c40cf0d8bff5de3
LuisLenzi <luisgustavolenzi@hotmail.com>
2021-05-20 01:20:06 -0300

    chore: creating loading spinner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import ptBR from 'date-fns/locale/pt-BR'
 import api from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 import styles from './home.module.scss';
+import { useEffect, useState } from 'react';
+import { Loading } from '../components/Loading';
 
 type Episode = {
   id: string;
@@ -23,81 +25,92 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const [showLoading, setShowLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowLoading(false);
+    }, 2500);
+  }, [])
+
   return (
-    <div className={styles.homepage}>
-      <section className={styles.latestEpisodes}>
-        <h2>Últimos lançamentos</h2>
-        <ul>
-          {
-            latestEpisodes.map(episode => {
-              return (
-                <li key={episode.id}>
-                  <Image
-                    width={200}
-                    height={200}
-                    src={episode.thumbnail}
-                    alt={episode.title}
-                    objectFit="cover"
-                    className={styles.image}
-                  />
-                  <div className={styles.episodeDetails}>
-                    <a href="">{episode.title}</a>
-                    <div className={styles.textBox}>
-                      <p>{episode.members}</p>
-                      <div className={styles.textContent}>
-                        <span>{episode.publishedAt}</span>
-                        <span className="duration">{episode.durationAsString}</span>
+    <>
+      <div className={styles.homepage}>
+        <section className={styles.latestEpisodes}>
+          <h2>Últimos lançamentos</h2>
+          <ul>
+            {
+              latestEpisodes.map(episode => {
+                return (
+                  <li key={episode.id}>
+                    <Image
+                      width={200}
+                      height={200}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                      className={styles.image}
+                    />
+                    <div className={styles.episodeDetails}>
+                      <a href="">{episode.title}</a>
+                      <div className={styles.textBox}>
+                        <p>{episode.members}</p>
+                        <div className={styles.textContent}>
+                          <span>{episode.publishedAt}</span>
+                          <span className="duration">{episode.durationAsString}</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className={styles.buttons}>
-                    <button type="button">
-                      <img src="assets/icons/play-button-icon.svg" alt="Startar episódio" />
-                    </button>
-                  </div>
-                </li>
-              )
-            })
-          }
-        </ul>
-      </section>
-      <section className={styles.allEpisodes}>
-        <h2>Todos os Podcasts</h2>
-        <ul>
-          {
-            allEpisodes.map(episode => {
-              return (
-                <li key={episode.id}>
-                  <Image
-                    width={75}
-                    height={75}
-                    src={episode.thumbnail}
-                    alt={episode.title}
-                    objectFit="cover"
-                    className={styles.image}
-                  />
-                  <div className={styles.episodeDetails}>
-                    <a href="">{episode.title}</a>
-                    <div className={styles.textBox}>
-                      <p>{episode.members}</p>
-                      <div className={styles.textContent}>
-                        <span>{episode.publishedAt}</span>
-                        <span className="duration">{episode.durationAsString}</span>
+                    <div className={styles.buttons}>
+                      <button type="button">
+                        <img src="assets/icons/play-button-icon.svg" alt="Startar episódio" />
+                      </button>
+                    </div>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </section>
+        <section className={styles.allEpisodes}>
+          <h2>Todos os Podcasts</h2>
+          <ul>
+            {
+              allEpisodes.map(episode => {
+                return (
+                  <li key={episode.id}>
+                    <Image
+                      width={75}
+                      height={75}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                      className={styles.image}
+                    />
+                    <div className={styles.episodeDetails}>
+                      <a href="">{episode.title}</a>
+                      <div className={styles.textBox}>
+                        <p>{episode.members}</p>
+                        <div className={styles.textContent}>
+                          <span>{episode.publishedAt}</span>
+                          <span className="duration">{episode.durationAsString}</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className={styles.buttons}>
-                    <button type="button">
-                      <img src="assets/icons/play-button-icon.svg" alt="Startar episódio" />
-                    </button>
-                  </div>
-                </li>
-              )
-            })
-          }
-        </ul>
-      </section>
-    </div>
+                    <div className={styles.buttons}>
+                      <button type="button">
+                        <img src="assets/icons/play-button-icon.svg" alt="Startar episódio" />
+                      </button>
+                    </div>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </section>
+      </div>
+      <Loading show={showLoading} />
+    </>
   )
 }
 
